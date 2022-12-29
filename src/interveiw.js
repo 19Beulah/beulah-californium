a589b15c42976edab17cef8228eb79185cc3b2cc
19Beulah <p.beulah5002@gmail.com>
2022-12-29 21:29:13 +0530

    added

---
+++ src/interveiw.js
@@ -0,0 +1,32 @@
+let obj={
+  name:"beulah" ,
+  age:55,
+  friends:["hello","hii"] ,
+  address:{
+    door_no:1-33,
+    place:"ongole"
+  }
+  }
+  // 1.console.log(obj.name)
+
+// 2.  obj.age=44
+//   console.log( obj.age)
+
+//3. let a=Object.values(obj)
+// console.log(a)
+
+// 4.let b=Object.keys(obj)
+// console.log(b)
+
+// 5.let c=Object.entries(obj)
+//  console.log(c)
+
+// let h=24
+// let i="24"
+// console.log(i===h)
+
+// let f={
+//   name:"fff"
+// }
+// let g=f
+// console.log({f}=={g}) 
